9ec110ef798676caeba778a55e23ceac7b9e2edb
liuhouer <654714226@qq.com>
2019-01-02 17:56:53 +0800

    lock

---
+++ src/main/java/cn/northpark/concurrency/aqs/packageInfo.java
@@ -0,0 +1,2 @@
+package cn.northpark.concurrency.aqs;
+//AbstractQueuedSynchronizer
---
+++ src/main/java/cn/northpark/concurrency/lock/LockExample6.java
@@ -0,0 +1,44 @@
+package cn.northpark.concurrency.lock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author w_zhangyang
+ * condition的队列和锁的队列可以相互协调
+ */
+@Slf4j
+public class LockExample6 {
+
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        Condition condition = reentrantLock.newCondition();
+
+        new Thread(() -> {
+            try {
+                reentrantLock.lock();
+                log.info("wait signal"); // 1
+                condition.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("get signal"); // 4
+            reentrantLock.unlock();
+        }).start();
+
+        new Thread(() -> {
+            reentrantLock.lock();
+            log.info("get lock"); // 2
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            condition.signalAll();
+            log.info("send signal ~ "); // 3
+            reentrantLock.unlock();
+        }).start();
+    }
+}
