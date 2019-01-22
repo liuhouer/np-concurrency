2f6de65f9156203600db049a63fe7e24210237cb
liuhouer <654714226@qq.com>
2019-01-22 18:50:13 +0800

    高并发demo

---
+++ src/main/java/cn/northpark/concurrency/threadPool/ThreadPoolExample2.java
@@ -0,0 +1,26 @@
+package cn.northpark.concurrency.threadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadPoolExample2 {
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("task:{}", index);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
